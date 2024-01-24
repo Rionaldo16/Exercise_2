805458df2048402ff5fb7ac09af36ae83c9d2d6c
Rionaldo16 <136539297+Rionaldo16@users.noreply.github.com>
2024-01-24 16:56:21 +0800

    menambahkan tugas

---
+++ members.js
@@ -0,0 +1,28 @@
+const members = [
+    {
+        "id": 1,
+        "nim": "123456789",
+        "nama": "Emor, Fransditho",
+        "nomor_telp": "081527205595"
+    },
+    {
+        "id": 2,
+        "nim": "987654321",
+        "nama": "Clif, Kevin",
+        "nomor_telp": "087654321098"
+    },
+    {
+        "id": 3,
+        "nim": "987654321",
+        "nama": "Siaman, Rionaldo",
+        "nomor_telp": "087654321098"
+    },
+    {
+        "id": 4,
+        "nim": "987654321",
+        "nama": "Tombeng, Valen",
+        "nomor_telp": "087654321098"
+    }
+];
+
+module.exports = members;
